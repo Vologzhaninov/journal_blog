0d23cf2e9df274eff45144c6c98fda968f70a8d0
Dmitry <d.vologzhaninov@gmail.com>
2025-09-28 21:40:32 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-document.getElementById('view-more').addEventListener('click', () => {
+const viewMore = document.getElementById('view-more')
+
+if (viewMore) {
+    viewMore.addEventListener('click', () => {
     const blogs = document.getElementsByClassName('blog-article')
     for (const blog of blogs){
         if (blog.classList.contains('invisible')){
             blog.classList.remove('invisible')
         }
+    }
+})
+}
+
+document.getElementById('menu-btn').addEventListener('click', () => {
+    const navBar = document.getElementById('nav-bar')
+    const menuBtn = document.getElementById('menu-btn')
+    const navUl = document.getElementById('nav-ul')
+    if (navBar.classList.contains('nav-bar-mobile')) {
+        navUl.classList.remove('nav-ul-mobile')
+        navBar.classList.remove('nav-bar-mobile')
+        menuBtn.classList.remove('menu-btn-mobile')
+        menuBtn.classList.toggle('menu-btn-hover')
+    } else {
+        navUl.classList.add('nav-ul-mobile')
+        navBar.classList.add('nav-bar-mobile')
+        menuBtn.classList.add('menu-btn-mobile')
+        menuBtn.classList.toggle('menu-btn-hover')
     }
 })
